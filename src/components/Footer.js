c2c101412a482a49ab947ba8d4c83401d9e4c1d2
Tanishq Kushwaha <tanishqkushwaha2002@gmail.com>
2024-08-01 14:05:24 +0530

    feat: updated the year in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
                 <Link to="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
                     <svg className="bi" width="30" height="24"></svg>
                 </Link>
-                <span className="text-muted">© 2021 Company, Inc</span>
+                <span className="text-muted">© {new Date().getFullYear()} Company, Inc</span>
             </div>
         </footer>
     )
